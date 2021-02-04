df9de2e87d1638f286df93cffc10211e758c9f6f
Christine Magnotta <christinemagnotta@CHRISTINEs-MacBook-Air.local>
2021-02-03 20:58:59 -0600

    trying to add markers.

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,14 @@
 // API key
 const API_KEY = "pk.eyJ1IjoiY21hZ25vdHRhIiwiYSI6ImNranhrZmIwNzA0dmMyb3BoMjljODN4ZWYifQ.sj5vtZtuQVtb9IBOUYhSNg";
 
+//earthquake levels
+
+//Class	Magnitude
+//Great	8 or more
+//Major	7 - 7.9
+//Strong	6 - 6.9
+//Moderate	5 - 5.9
+//Light	4 - 4.9
+//Minor	3 -3.9
+
+//https://nuchidatapt09-sun2423.slack.com/files/U01G8L94MHU/F01KL36LPAM/leaflet.extra-markers.min.js
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,58 +1,75 @@
 function createMap(earthquakes) {
-console.log(earthquakes);
-
-var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1,
-  accessToken: API_KEY
-});
-
-var baseMaps = {
-    "Light Map": lightmap
-  };
-
-var overlayMaps = {
-    "Earthquakes": earthquakes
-  };
-
-var map = L.map("mapid").setView([38.5816, -121.4944], 3)
-
-lightmap.addTo(map);
-
-for (var i = 0; i < earthquakes.length; i++) {
-  earthquakes[i].addTo(map);
-}
-
-//L.control.layers(baseMaps, overlayMaps).addTo(map);
-}
-
-
-function createMarkers(response) {
-    console.log(response);
-    var coordinates = response.features[0].geometry.coordinates;
-    var magnitude = response.features[0].properties.mag;
-    console.log(coordinates);
-    console.log(magnitude);
-    
-    var quakeMarkers = [];
+  //console.log(earthquakes);
   
-    
-    for (var i = 0; i < response.features.length; i++) {
-      const [lon, lat] = response.features[i].geometry.coordinates;
-console.log(lat)
-      var quakeMarker = L.marker([lat, lon]);
-        //.bindPopup("<h3>" + response.features[i].properties.place + "<h3><h3>Magnitude: " + response.features[i].properties.mag + "</h3>");
+  var lightmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+  });
   
-      
-      quakeMarkers.push(quakeMarker);
-    }
+  var baseMaps = {
+      "Light Map": lightmap
+    };
   
-    
-    createMap(quakeMarkers);
+  var overlayMaps = {
+      "Earthquakes": earthquakes
+    };
+  
+  var map = L.map("mapid").setView([38.5816, -121.4944], 3)
+  
+  lightmap.addTo(map);
+  
+  for (var i = 0; i < earthquakes.length; i++) {
+    earthquakes[i].addTo(map);
   }
   
+  //L.control.layers(baseMaps, overlayMaps).addTo(map);
+  }
+  
+  
+  function createMarkers(response) {
+      //console.log(response);
+      var coordinates = response.features[0].geometry.coordinates;
+      var magnitude = response.features[0].properties.mag;
+      //console.log(coordinates);
+      //console.log(magnitude);
+      
+      var quakeMarkers = [];
+    
+      
+      for (var i = 0; i < response.features.length; i++) {
+        const [lon, lat] = response.features[i].geometry.coordinates;
+  //console.log(lat)
+        var quakeMarker = L.marker([lat, lon]);
+        console.log(magnitude);
+        //.bindPopup("<h3>" + response.features[i].properties.place + "<h3><h3>Magnitude: " + response.features[i].properties.mag + "</h3>");
+        var color = "";
+        if (magnitude[i] > 8) {
+          color = "yellow";
+        }
+        else if (magnitude[i] > 7) {
+          color = "blue";
+        }
+        else if (magnitude[i] > 2) {
+          color = "green";
+        }
+        else {
+          color = "red";
+        }
+        
+        quakeMarkers.push(quakeMarker);
+      }
+ 
+    
 
-  d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
+      createMap(quakeMarkers);
+    }
+    
+   
+    
+
+
+    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers);
